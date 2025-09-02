cc73b18f280d0cc5ad5543ec70620ed2a1d8530d
SahilGite4fox <sahilgite.4fox@gmail.com>
2025-09-02 10:46:20 +0530

    home page desktop

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,9 +2,74 @@ $(document).ready(function () {
 
   $('.menu-btn, .nav__link').on('click', function () {
     $('.menu-btn, .nav').toggleClass('active');
-});
+  });
 
+  // Slick Slider
+  $('.teamSlideBox').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
+  $('.gallerySlider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
   // Sticky Header
   $(window).on("scroll", function () {
